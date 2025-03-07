a1e5242b09016dcdc9d350444cfd35c5657c8601
aniketangre <aniketangre.edu@gmail.com>
2025-03-07 17:19:15 +0100

    Initial iteration

---
+++ src/containers/skills/ScientificComputingImg.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class ScientificComputingImg extends Component {
+  render() {
+    return (
+      <img
+        src={require("../../assets/images/scientific_comp.png")}
+        alt="Scientific Computing"
+      />
+    );
+  }
+}
